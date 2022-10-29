fadc29bd093ab1b028db8a8bb6472f0b314b27c2
suphinya <68013766+suphinya@users.noreply.github.com>
2022-10-29 18:21:22 +0700

    Delete medapp_demo1-main directory

--- medapp_demo1-main/medapp/carry.py
+++
@@ -1,3 +0,0 @@
-a = {"name": "onpinya", "surname": "phok"}
-b = a["name"]
-print(b)
--- medapp_demo1-main/medapp/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Elder
-
-class ImageForm(forms.ModelForm):
-    """Form for the image model"""
-    class Meta:
-        model = Elder
-        fields = ('image',)
